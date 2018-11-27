7919a4f78e67b07e6fcbbb2abd1405f7ac45f91b
Hagai Reuveni <reuveni.hagai@oath.com>
2018-11-27 17:13:38 +0200

    SCROLL TOP

--- src/router.js
+++ src/router.js
@@ -27,5 +27,8 @@ export default new Router({
       name: 'ProductDetails',
       component: ProductDetails
     }
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 })
